bf2f84c28b6acca77953e9b3cf9fd2d63a64e6dc
EdiedRamos <edieisa22@gmail.com>
2024-05-15 04:23:58 -0500

    chore: add Product entity

---
+++ src/main/java/com/platzi/market/persistence/entity/Product.java
@@ -0,0 +1,85 @@
+package com.platzi.market.persistence.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "product_id")
+    private Long productId;
+
+    @Column(name = "category_id")
+    private Long categoryId;
+
+    private String name;
+
+    private String barcode;
+
+    private Double price;
+
+    @Column(name = "stock_quantity")
+    private Integer stockQuantity;
+
+    private Boolean state;
+
+    public Product() {
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public Long getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Long categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBarcode() {
+        return barcode;
+    }
+
+    public void setBarcode(String barcode) {
+        this.barcode = barcode;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Integer getStockQuantity() {
+        return stockQuantity;
+    }
+
+    public void setStockQuantity(Integer stockQuantity) {
+        this.stockQuantity = stockQuantity;
+    }
+
+    public Boolean getState() {
+        return state;
+    }
+
+    public void setState(Boolean state) {
+        this.state = state;
+    }
+}
