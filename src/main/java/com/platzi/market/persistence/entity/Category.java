027c236095ee3ab222eb16c5d232c3bc6b31e837
EdiedRamos <edieisa22@gmail.com>
2024-05-15 04:38:07 -0500

    chore: add Category entity

---
+++ src/main/java/com/platzi/market/persistence/entity/Category.java
@@ -0,0 +1,45 @@
+package com.platzi.market.persistence.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "categories")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "category_id")
+    private Long categoryId;
+
+    private String description;
+
+    private Boolean state;
+
+    public Category() {
+
+    }
+
+    public Long getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Long categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Boolean getState() {
+        return state;
+    }
+
+    public void setState(Boolean state) {
+        this.state = state;
+    }
+}
