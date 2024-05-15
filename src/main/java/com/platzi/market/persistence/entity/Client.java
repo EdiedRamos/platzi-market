09d9b686540c98d97cf83ecd2df9d3f4a8d42a53
EdiedRamos <edieisa22@gmail.com>
2024-05-15 04:35:31 -0500

    chore: add Client entity

---
+++ src/main/java/com/platzi/market/persistence/entity/Client.java
@@ -0,0 +1,77 @@
+package com.platzi.market.persistence.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "clients")
+public class Client {
+
+    @Id
+    @Column(name = "client_id")
+    private String clientId;
+
+    private String name;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    private Long mobile;
+
+    private String address;
+
+    private String email;
+
+    public Client() {
+    }
+
+    public String getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(String clientId) {
+        this.clientId = clientId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Long getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(Long mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
